b8005f7097bfcba44a6596092a153badf7ad8c97
Piotr Kosmowski <pkosmowski553@gmail.com>
2024-12-22 12:36:59 +0100

    calculator with css

--- my-calculator/src/App.tsx
+++ my-calculator/src/App.tsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Device from './components/Device'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>hej</div>
+    <div className='main'>
+      <Device />
+    </div>
   )
 }
 
---
+++ my-calculator/src/components/Device.tsx
@@ -0,0 +1,35 @@
+
+
+export default function Device() {
+    return (
+        <div className="container-calc">
+            <div className="in-out-numbs">2512</div>
+            <div className="buttons">
+                <div>%</div>
+                <div>CE</div>
+                <div>C</div>
+                <div>DEL</div>
+                <div>1/x</div>
+                <div>x^2</div>
+                <div>sqrt(x)</div>
+                <div>/</div>
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+                <div>x</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>-</div>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>+</div>
+                <div>+/-</div>
+                <div>0</div>
+                <div>.</div>
+                <div>=</div>
+            </div>
+        </div>
+    )
+}
