5fbbabee3280c7ee42cd1e07aff7ddc68e3d18c8
Piotr Kosmowski <pkosmowski553@gmail.com>
2024-12-25 19:08:42 +0100

    modified questionsHTML page

--- frontend-quiz-app/src/app/pages/questionsHTML/page.js
+++ frontend-quiz-app/src/app/pages/questionsHTML/page.js
@@ -1,47 +1,57 @@
+'use client';
+
 let questions = require('../../data.json')
 
 let i = 0
 let actual_question = questions.quizzes[0].questions[i]
 
-export default function HTMLQuestions() {
 
+function Submit() {
+    const next = () => {
+        i+=1
+        console.log(i);
+        
+    }
     return (
-        <div className='flex gap-4'>
-            <Question question={actual_question.question} />
-            <Answers/>
-        </div>
+        <button onClick={next}>CLICK ME!</button>
     )
 }
 
 function Answers() {
-    actual_question.options.map((ans) => {
-        <button>{ans}</button>
-    });
-    
     return (
-        <div className='container-answers'>
-            <ol className='flex flex-col'>
+        <div className='container-answers flex flex-col gap-10'>
                 {actual_question.options.map((ans, key) => {
-                    return <button key={key}>{ans}</button>
-                })}
-            </ol>
-            
+                    return <Answer text={ans} key={key} />
+                })}        
         </div>
     )
 }
 
 function Answer({ text }) {
-    return <button></button>
+    return <button className='bg-red-600 p-6 rounded'>{text}</button>
 }
 
 function Question({question}) {
     return (
-        <div>{question}</div>
+        <div>
+            <div>Question {i+1} of 10</div>
+            <div>{question}</div>
+        </div>
     )
 }
 
-function SubmitAnswer() {
+
+export default function HTMLQuestions() {
     return (
-        <button>SUBMIT</button>
+        <div className='flex gap-20'>
+            <div className='container-left'>
+                <Question question={actual_question.question} />
+            </div>
+            <div className='container-right flex flex-col gap-20'>
+                <Answers/>
+                <Submit />
+            </div>
+
+        </div>
     )
 }
